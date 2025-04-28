41652b1c52daf90e5529f10bb48fee855fb88a48
Darylzz <cheer_bo50@hotmail.com>
2025-04-28 13:31:15 +0700

    setting formConfig in page for test

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,39 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { FormConfig, HasButtonInForm } from "./component/controls/FormConfig";
+import FormComponent from "./component/controls/FormComponent";
+import { ControlType } from "./component/controls/FormControlType";
+import { Button } from "primereact/button";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const formConfig: FormConfig[] = [
+    {
+      CTRL_KEY: "name",
+      CTRL_TYPE: ControlType.INPUT,
+      LABEL: "Name",
+      PLACEHOLDER: "Enter your name",
+      REQUIRED: true,
+    },
+    // {
+    //   CTRL_KEY: "number",
+    //   CTRL_TYPE: ControlType.NUMBER,
+    //   LABEL: "number",
+    //   PLACEHOLDER: "Enter your number",
+    //   REQUIRED: true,
+    // },
+  ];
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const formConfigButton: HasButtonInForm = {
+    HAS_BUTTON_SUBMIT: true,
+    HAS_BUTTON_RESET: true,
+  };
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <FormComponent formConfig={formConfig} onSubmit={onSubmit} formConfigButton={formConfigButton}>
+      {/* <Button type='submit' label='submit'></Button> */}
+    </FormComponent>
   );
 }
