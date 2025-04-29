216d39c7e6758322f4c9546f771deefe0397bf87
Darylzz <cheer_bo50@hotmail.com>
2025-04-29 13:33:13 +0700

    insert new type on FormConfig for config the control form, insert new interface SetFormValue for function set form value

--- app/component/controls/FormConfig.ts
+++ app/component/controls/FormConfig.ts
@@ -1,10 +1,10 @@
-import { ControlType } from "./FormControlType";
+import { ControlType } from './FormControlType';
 
 export interface FormConfig {
   CTRL_KEY: string;
   CTRL_TYPE: ControlType;
   LABEL: string;
-  REQUIRED?: boolean;
+  REQUIRED: boolean;
   PLACEHOLDER?: string;
   OPTION?: any[]; // for select, radio
   OPTION_LABEL?: string;
@@ -13,12 +13,23 @@ export interface FormConfig {
   DEFAULT_VALUE?: any;
   SHOW_ICON?: boolean;
   SHOW_ICON_CLEAR?: boolean;
-  DISABLED?: boolean;
+  DISABLED: boolean;
   ERROR_MESSAGE?: string;
   AUTO_RESIZE?: boolean;
+  MIN?: number;
+  MAX?: number;
+  EMPTY_MESSAGE?: string;
+  OPTIONS?: any[];
+  WIDTH?: string;
+  onChange?: (value: any) => void;
 }
 
 export interface HasButtonInForm {
   HAS_BUTTON_SUBMIT?: boolean;
   HAS_BUTTON_RESET?: boolean;
+}
+
+export interface SetFormValue {
+  name: string;
+  value: any;
 }
