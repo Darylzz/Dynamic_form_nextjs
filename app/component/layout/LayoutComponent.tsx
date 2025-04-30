6376154d751d304500f31df5f99d05a57e15c1c0
Darylzz <cheer_bo50@hotmail.com>
2025-04-30 10:27:31 +0700

    adjust layout component

---
+++ app/component/layout/LayoutComponent.tsx
@@ -0,0 +1,26 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import { Button } from 'primereact/button';
+
+export default function LayoutComponent({ ...props }) {
+  const { children } = props;
+  const router = useRouter();
+
+  const handleClickNavigate = (key: string) => {
+    if (key === 'playground') {
+      router.push('/playground');
+    } else {
+      router.push('login');
+    }
+  };
+
+  return (
+    <>
+      <div className='flex gap-3 p-3'>
+        <Button label='Playground' onClick={() => handleClickNavigate('playground')} />
+        <Button label='Login' onClick={() => handleClickNavigate('login')} />
+      </div>
+      <div className='p-3'>{children}</div>
+    </>
+  );
+}
